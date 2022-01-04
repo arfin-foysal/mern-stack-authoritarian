c10defbff8690a6af083d2c7b418f8deca3e893a
Arfin Foysal <arfinfoysalbd@gmail.com>
2022-01-05 00:03:08 +0600

    backend forget complitr

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,9 +14,9 @@ const registration = async (req, res) => {
   });
   try {
     const saveUser = await user.save();
-    res.status(200).json({ mess: "registration sucessfull"});
+    res.status(200).json({ mess: "registration sucessfull" });
   } catch (error) {
-    res.status(400).json({ mess: "this is server err"});
+    res.status(400).json({ mess: "this is server err" });
   }
 };
 
@@ -42,7 +42,71 @@ const login = async (req, res) => {
   res.status(200).json({ token: Token });
 };
 
+// post
+const forgotPassword = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email});
+
+  if (!user) {
+    res.send("email not registrared");
+    return;
+  }
+  const secret = process.env.TOKEN + user.password;
+  const payloade = {
+    email: user.email,
+    id: user._id,
+  };
+  const token = jwt.sign(payloade, secret, { expiresIn: "15m" });
+  const link = `localhost:5000/api/reset-password/${user._id}/${token}`;
+  console.log(link);
+  res.send("password send email");
+};
+
+// get
+const resetPassword = async (req, res) => {
+  const { id, token } = req.params;
+  const user = await User.findOne({ _id:id });
+
+  if (id !== user._id) {
+    req.send("invalated id");
+  }
+
+  const secret = process.env.TOKEN + user.password;
+  try {
+    const payload = jwt.verify(token, secret);
+    res.render("reset-password", { email: user.email });
+  } catch (error) {
+    console.log(error.messages);
+    res.send(error.messages);
+  }
+};
+
+const postResetPassword = async(req, res) => {
+  const { id, token } = req.params;
+  const { password, confirmpassword } = req.body;
+  const user = await User.findOne({ _id: id });
+  if (id !== user._id) {
+    req.send("invalated id");
+  }
+  const secret = process.env.TOKEN + user.password;
+  const payload = jwt.verify(token, secret);
+  try {
+    if (password === confirmpassword) {
+      const user = new User({
+        password: req.body.password,
+      });
+      user.save();
+      res.send("Password Change Sucessfull");
+    }
+  } catch (error) {
+    console.log(error.messages);
+    res.send(error.messages);
+  }
+};
+
 module.exports = {
   registration,
   login,
+  forgotPassword,
+  resetPassword,
+  postResetPassword,
 };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { registration, login } = require('../controllers/userController');
+const { registration, login, resetPassword, forgotPassword, postResetPassword } = require('../controllers/userController');
 
 const router = express.Router()
 
 router.post('/registration',registration)
 router.post('/login',login)
+router.post('/forgot-password',forgotPassword)
+router.get('/reset-password/:id/:token',resetPassword)
+// router.post('/reset-password/:id/:token',postResetPassword)
 
 module.exports=router;
